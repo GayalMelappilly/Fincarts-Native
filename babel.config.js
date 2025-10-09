75748df697e63bc9d0f4168b5819c909fecb9c97
Gayal Melappilly <gayalsunil@gmail.com>
2025-10-10 04:13:03 +0530

    fix: clean dependencies for EAS build

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,10 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }]
-    ],
+    presets: ['babel-preset-expo'],
     plugins: [
-      "react-native-reanimated/plugin", // This should be last
+      'nativewind/babel',
+      'react-native-reanimated/plugin',
     ],
   };
 };
