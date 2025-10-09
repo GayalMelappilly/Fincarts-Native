7614400b887c3b9ad482a8820a6c6bd7434e641c
Gayal Melappilly <gayalsunil@gmail.com>
2025-10-10 04:21:31 +0530

    fix: configure NativeWind preset for Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,10 @@
 module.exports = {
   content: [
     "./app/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}"
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,jsx,ts,tsx}"
   ],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
