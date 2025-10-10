0c316a3e0ccf642eacef56f07d7213b115b99cf8
Gayal Melappilly <gayalsunil@gmail.com>
2025-10-10 05:32:28 +0530

    fix: remove babel-plugin-module-resolver from babel config

--- babel.config.js
+++ babel.config.js
@@ -2,26 +2,6 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          alias: {
-            '@': './',
-          },
-          extensions: [
-            '.ios.js',
-            '.android.js',
-            '.js',
-            '.jsx',
-            '.json',
-            '.tsx',
-            '.ts',
-          ],
-        },
-      ],
-      'react-native-reanimated/plugin',
-    ],
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
